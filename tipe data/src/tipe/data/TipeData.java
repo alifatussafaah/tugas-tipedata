8f78650dac3bd4c226be95751c947993c7b7ef0c
alifatussafaah <145674416+alifatussafaah@users.noreply.github.com>
2023-09-21 21:44:52 +0700

    Add files via upload

---
+++ tipe data/src/tipe/data/TipeData.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tipe.data;
+
+/**
+ *
+ * @author DELL
+ */
+public class TipeData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //tipe data string
+        String nama, alamat, bulan, nik;
+  
+        //tipe data int
+        int tanggallahir, tahun;
+        
+        //tipe data double
+        double ipk;
+        
+        //tipe data char
+        char jeniskelamin;  
+        
+        //variable
+        nama = "zayn malik ";
+        alamat = "bradford";
+        nik = "230720102603";
+        tanggallahir = 12;
+        bulan = "januari";
+        tahun = 1993;
+        ipk = 3.89;
+        jeniskelamin = 'l';
+        
+        System.out.println("nama         = " + nama);
+        System.out.println("alamat       = " + alamat); 
+        System.out.println("nik          = " + nik);
+        System.out.println("tanggallahir = " + tanggallahir);
+        System.out.println("bulan        = " + bulan);
+        System.out.println("tahun        = " + tahun);
+        System.out.println("ipk          = " + ipk);
+        System.out.println("jeniskelamin = " + jeniskelamin);
+    }
+}
